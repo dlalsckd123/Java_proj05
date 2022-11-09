2c6c6a2e33ab81dad32d1da9456a353e2ffd4b1d
Minchang Lee <dlalsckd1@naver.com>
2022-11-09 11:37:59 +0900

    천번째 커밋

---
+++ src/chapter01/ex01/Test_Class.java
@@ -0,0 +1,11 @@
+package chapter01.ex01;
+
+public class Test_Class {
+public static void main(String[] args) {
+	//main 컨트롤 + 스페이스
+	
+	// 패키지 생성, 클래스 생성, 클래스를 패키지 사이로 이동, 하위 패키지 생성
+	// 기본적으로 JSP Spring을 구성할떄 3단계의 Package가 기본적으로 등록
+	// com.naver.www : 클래스 저장 
+}
+}
